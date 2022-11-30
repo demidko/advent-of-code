80ae385cabbe3d74ddb4d3fdfad38fb0843b6f45
Daniel Demidko <demidko@farpost.com>
2022-12-01 03:44:53 +0500

    wip...

--- src/main/kotlin/com/adventofcode/Day.kt
+++ src/main/kotlin/com/adventofcode/Day.kt
@@ -1,9 +1,9 @@
 package com.adventofcode
 
 fun main() {
-  val resource = ::main.javaClass.getResourceAsStream("/day1.example.txt")!!
-  val reader = resource.bufferedReader()
-  reader.forEachLine(::println)
+  ::main
+    .javaClass
+    .getResourceAsStream("/day1.example.txt")!!
+    .bufferedReader()
+    .forEachLine(::println)
 }
-
-
