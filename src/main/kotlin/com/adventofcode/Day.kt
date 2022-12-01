fa0b4b3d79289fb5a23637954a398cc9a3ca5b80
Daniel Demidko <demidko@farpost.com>
2022-12-01 10:18:54 +0500

    upd

--- src/main/kotlin/com/adventofcode/Day.kt
+++ src/main/kotlin/com/adventofcode/Day.kt
@@ -1,9 +1,22 @@
 package com.adventofcode
 
+import com.adventofcode.Day.solution
+
+object Day {
+  fun process(l: String) {
+    TODO()
+  }
+
+  fun solution(): Long {
+    TODO()
+  }
+}
+
 fun main() {
   ::main
     .javaClass
-    .getResourceAsStream("/day1.example.txt")!!
+    .getResourceAsStream("/input")!!
     .bufferedReader()
-    .forEachLine(::println)
+    .forEachLine(Day::process)
+  println(solution())
 }
